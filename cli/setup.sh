ce4f724a60635cdb12aa927422b2eddf93a85e06
Đoàn Quang Tuấn <doantuan998@gmail.com>
2023-08-30 17:36:57 +0700

    Update setup.sh

--- cli/setup.sh
+++ cli/setup.sh
@@ -1,6 +1,6 @@
-GROUP="RG"
-LOCATION="eastus"
-WORKSPACE="AzureMLWorkspace"
+GROUP="towardsdatascience_tutorial_rg"
+LOCATION="eastus2"
+WORKSPACE="towardsdatascience_tutorial_ws"
 
 az configure --defaults group=$GROUP workspace=$WORKSPACE location=$LOCATION
 
